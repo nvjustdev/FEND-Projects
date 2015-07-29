6297e97b2f5a6c2f338b5ef0442cbbb09e54b2dc
nirmala-mist <nirmala@mistsys.com>
2015-07-29 14:44:54 -0700

    Added List View functionality
    - When a name is chosen from the list, all other markers are hidden,
    the chosen marker’s info window is shown
    - Closing the info window resets everything

--- Project 5/js/app.js
+++ Project 5/js/app.js
@@ -23,7 +23,8 @@ var School = function (name, type, latitude, longitude, score, review) {
 function appViewModel() {
 	var self = this;
 	var latitude, longitude, map, schools, searchType, defaultZipCode,
-		mapMarkers, isInfoWindowVisible, shouldShowSearchSection;
+		mapMarkers, isInfoWindowVisible, shouldShowSearchSection, currentMarker, currentMarkerInitial,
+		classifiedMapMarkers, isColorMarkerShown, isShowingAll, toggleButtonName, currentSchoolIndex;
 
 	self.defaultZipCode = "95014";
 
@@ -38,10 +39,21 @@ function appViewModel() {
 	self.searchType = ko.observable(''); //The type of the school that is used for searching the map
 	self.schools = ko.observableArray([]); //The schools in the specified neighborhood
 	self.mapMarkers = ko.observableArray([]); //The markers corresponding to the schools' location
+	self.classifiedMapMarkers = ko.observableArray([]); //The color coded markers corresponding to the schools' location
 
-	self.isInfoWindowVisible = ko.observable(false);
-	self.shouldShowSearchSection = ko.observable(true);
+	self.isInfoWindowVisible = ko.observable(false); //Tracks if info window is visible
+	self.shouldShowSearchSection = ko.observable(true); //Tracks if the search section should be shown or not
+	self.isColorMarkerShown = ko.observable(false); //Tracks if the color coded markers are shown
+	// self.isShowingAll = ko.observable(true); //Tracks if all the markers are shown
 
+	self.currentMarker = ko.observable(); //Tracks the current clicked marker
+	self.currentMarkerInitial = ko.observable(); //Tracks the current clicked marker's Initial
+
+	self.toggleButtonName = ko.observable("Show Colored Markers"); //Toggles the button name
+
+	self.currentSchoolIndex = ko.observable(); //Tracks the current clicked school's index
+
+	//Compute the first initial of the school type
 	self.schoolTypeInitial = ko.computed(function() {
 		return ko.utils.arrayMap(self.schools(), function(item) {
             return item.schoolType.slice(0, 1);
@@ -65,15 +77,23 @@ function appViewModel() {
 		makeMarkers(self.schools());
 	}
 
+	/* Clear any filtering */
+	//Show all the schools
+	this.clearFilter = function() {
+		self.searchType(''); //No search type
+		$.each(self.schools(), function(index) {
+			self.mapMarkers()[index].marker.setVisible(true); //Show all the markers
+		});
+		// self.isShowingAll(true); //Set isShowingAll to be true
+	};
+
 	/* Search and Filter actions */
 	//If the schoolType matches the searchtype, then show the school, else hide the school
 	//If there's no search text, show all the schools
 	//Changing both the type of the object and the search string to be the same case
 	this.filterSchoolsByType = function() {
-		console.log(self.searchType());
 		$.each(self.schools(), function(index) {
 			if (self.searchType().length > 0) {
-				console.log(self.searchType())
 				if (self.schools()[index].schoolType.toLowerCase() === self.searchType().toLowerCase()) {
 					self.mapMarkers()[index].marker.setVisible(true);
 				} else {
@@ -85,13 +105,45 @@ function appViewModel() {
 		});
 	};
 
-	//Show all the schools
-	this.clearFilter = function() {
-		self.searchType('');
+	/* List View */
+	//Get the index of the clicked school
+	this.setCurrentSchoolIndex = function(clickedSchoolIndex) {
+		self.currentSchoolIndex(clickedSchoolIndex());//Set the index in the variable
+		filterSchoolsByIndex(self.currentSchoolIndex());//Trigger filtering
+	}
+
+	//Show only the clicked school
+	function filterSchoolsByIndex(clickedSchoolIndex) {
 		$.each(self.schools(), function(index) {
-			self.mapMarkers()[index].marker.setVisible(true);
+			self.mapMarkers()[index].marker.setVisible(false); //Hide all the markers
 		});
-	};
+
+		var currentMarker = self.mapMarkers()[clickedSchoolIndex].marker; //Get the marker of interest into a variable
+		currentMarker.setVisible(true); //Show the marker of interest
+		google.maps.event.trigger(currentMarker, 'click'); //Trigger clicked event to show infoWindow
+		// self.isShowingAll(false); //Setting the isShowingAll to be false
+	}
+
+	/* Color Coding by School Type */
+	//Show/Hide color coded markers
+	this.toggleColorCodedMarkers = function() {
+		//We want to show the color coded markers
+		if (self.toggleButtonName() === "Show Colored Markers") {
+			self.isColorMarkerShown(true); //Set isColorMarkerShown to be true
+			self.toggleButtonName("Show Clickable Markers"); //Change the name of the button
+			$.each(self.mapMarkers(), function(index) {
+				self.mapMarkers()[index].marker.setVisible(false); //Hide the clickable markers
+				self.classifiedMapMarkers()[index].marker.setVisible(true); //Show the colored markers
+			});
+		} else {
+			self.isColorMarkerShown(false); //Set isColorMarkerShown to be false
+			self.toggleButtonName("Show Colored Markers"); //Change the name of the button
+			$.each(self.mapMarkers(), function(index) {
+				self.mapMarkers()[index].marker.setVisible(true); //Show the clickable markers
+				self.classifiedMapMarkers()[index].marker.setVisible(false); //Hide the colored markers
+			});
+		}
+	}
 
 	/* Map related actions */
 	//Initialize the map
@@ -132,6 +184,34 @@ function appViewModel() {
 	        	visible: true
 	      	});
 
+	      	var iconImage;
+
+	      	switch (currentSchool.schoolType) {
+	      		case 'Elementary':
+	      			iconImage = 'images/blue-dot.png';
+	      			break;
+	      		case 'Middle':
+	      			iconImage = 'images/yellow-dot.png';
+	      			break;
+	      		case 'High':
+	      			iconImage = 'images/green-dot.png';
+	      			break;
+	      		case 'Private':
+	      			iconImage = 'images/purple-dot.png';
+	      			break;
+	      		case 'K-8':
+	      			iconImage = 'images/red-dot.png';
+	      			break;
+	      	}
+
+	      	var colorCodedMarker = new google.maps.Marker({
+	      		position: geoLocation,
+	        	map: map,
+	        	title: currentSchool.schoolName,
+	        	icon: iconImage,
+	        	visible: false
+	      	});
+
 	      	//Set a label of the marker (The first initial of the school)
 	      	marker.setLabel(self.schoolTypeInitial()[index]);
 
@@ -143,27 +223,35 @@ function appViewModel() {
 	      	//Set the isInfoWindowVisible to be false upon info window close
 	      	//Set the shouldShowSearchSection to be true
 	      	google.maps.event.addListener(infowindow,'closeclick',function(){
-	      		self.isInfoWindowVisible(false);
-	      		self.shouldShowSearchSection(true);
+	      		self.isInfoWindowVisible(false); //Hide the info window
+	      		self.shouldShowSearchSection(true); //Show the search section
+	      		self.currentMarker().setAnimation(null); //Stop the animation of the marker
+	      		self.currentMarker().setLabel(self.currentMarkerInitial()); //Reset the marker's label
+	      		self.clearFilter();
 			});
 
 	      	//Set the map for the marker
 	      	marker.setMap(map);
+	      	colorCodedMarker.setMap(map);
 
 	      	//Push the marker to the mapMarkers array
       		self.mapMarkers.push({marker: marker, content: contentString});
 
+      		//Push the color coded marker to the classifiedMapMarkers array
+      		self.classifiedMapMarkers.push({marker: colorCodedMarker, type: currentSchool.schoolType});
+
 		    //Marker Click Action - Show the School Name and Scores
 		    google.maps.event.addListener(marker, 'click', function() {
-		    	//Open the infowindow
-		        infowindow.open(map, marker);
-
-		        //Set the isInfoWindowVisible to be true - to open review section
-		        self.isInfoWindowVisible(true);
-
-		        //Set the shouldShowSearchSection to be false - to close search section
-		        self.shouldShowSearchSection(false);
+		    	self.currentMarker(marker); //Set the current marker
+		    	self.currentMarkerInitial(self.currentMarker().getLabel()); //Get the current marker's label
+		    	self.currentMarker().setLabel(""); //Set the current marker's label to be nothing
+		    	self.currentMarker().setAnimation(google.maps.Animation.BOUNCE); //Set animation of the marker
+		        infowindow.open(map, marker); //Open the infowindow
+		        self.isInfoWindowVisible(true); //Set the isInfoWindowVisible to be true - to open review section
+		        self.shouldShowSearchSection(false); //Set the shouldShowSearchSection to be false - to close search section
 		    });
+
+		    colorCodedMarker.setClickable(false); //color coded marker shouldn't be clickable
 		 });
 	}
 
@@ -183,13 +271,6 @@ function appViewModel() {
 
 ko.applyBindings(new appViewModel());
 
-// ko.components.register('message-editor', {
-//     viewModel: function(params) {
-//         self.searchType = ko.observable(params);
-//     },
-//     template: ''
-// });
-
 /* Keyboard */
 ko.bindingHandlers.executeOnEnter = {
     init: function (element, valueAccessor, allBindings, viewModel) {
@@ -204,6 +285,28 @@ ko.bindingHandlers.executeOnEnter = {
         });
     }
 };
+
+	// self.addLang = function(event, ui){
+ //        $(event.target).val("");
+ //        console.log(ui.item);
+ //        return false;
+ //    }
+
+// ko.components.register('message-editor', {
+//     viewModel: function(params) {
+//         self.searchType = ko.observable(params);
+//     },
+//     template: ''
+// });
+
+// ko.bindingHandlers.ko_autocomplete = {
+//     init: function (element, params) {
+//         $(element).autocomplete(params());
+//     },
+//     update: function (element, params) {
+//         $(element).autocomplete("option", "source", params().source);
+//     }
+// };
 
 
 	// var geocoder = new google.maps.Geocoder();
